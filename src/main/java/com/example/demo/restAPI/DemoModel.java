69c55920f3b575434862553acf155cfca42734f9
Shelby mcgarrah <shelbymcgarrah@Shelbys-MBP.attlocal.net>
2019-05-15 19:37:59 -0500

    added post with request body

---
+++ src/main/java/com/example/demo/restAPI/DemoModel.java
@@ -0,0 +1,22 @@
+package com.example.demo.restAPI;
+
+public class DemoModel {
+
+	private String testString;
+
+	public String getTestString() {
+		return testString;
+	}
+
+	public void setTestString(String testString) {
+		this.testString = testString;
+	}
+
+	@Override
+	public String toString() {
+		return "DemoModel [testString=" + testString + "]";
+	}
+	
+	
+	
+}
--- src/main/java/com/example/demo/restAPI/DemoRestController.java
+++ src/main/java/com/example/demo/restAPI/DemoRestController.java
@@ -2,6 +2,8 @@ package com.example.demo.restAPI;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +15,10 @@ public class DemoRestController {
 	@GetMapping(value="/pathParam/{value}")
 	public String pathParameter(@PathVariable("value") int val) {
 		return "Path parameter value : " + val;
+	}
+	
+	@PostMapping(value="/simplePost")
+	public String simplePost(@RequestBody DemoModel model) {
+		return "You sent request body with : " + model.toString();
 	}
 }
